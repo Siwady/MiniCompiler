aa58a87a1f8f197af71ba7d6b7aa7a6b7b943fcc
Siwady <siwady0908@unitec.edu>
2015-07-22 20:32:24 -0600

    first commit

---
+++ MiniCompiler/Lexer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniCompiler
+{
+    class Lexer
+    {
+        public Token GetToken()
+        {
+            return null;
+        }
+    }
+}
---
+++ MiniCompiler/Token.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MiniCompiler
+{
+    internal enum TokenType
+    {
+
+    }
+
+    class Token
+    {
+        public TokenType Type { set; get; }
+        public string Lexeme { set; get; }
+        public int Column { set; get; }
+        public int Row { set; get; }
+    }
+}
